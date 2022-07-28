517ea67977d591ed8385e86d79cc022c76a67149
alanrps <alanrodrigo636@gmail.comn>
2022-07-27 22:43:25 -0300

    Add unit tests

--- src/repositories/prisma/prisma-feedbacks-repository.ts
+++ src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -2,13 +2,13 @@ import { prisma } from "../../prisma";
 import { FeedbackRepository, FeedbackCreateData } from "../Feedbacks-repository";
 
 export class PrismaFeedbacksRepository implements FeedbackRepository {
-    async create({ type, comment, screenshot }: FeedbackCreateData){
-        await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    })
+        async create({ type, comment, screenshot }: FeedbackCreateData){
+            await prisma.feedback.create({
+            data: {
+                type,
+                comment,
+                screenshot,
+            }
+        });
     }
 }
